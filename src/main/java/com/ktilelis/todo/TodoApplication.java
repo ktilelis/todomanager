3303bb7b66a93f3f8f1c153d6ef1b1c50f48ad26
Kostas Tilelis <kostas.tilelis@gmail.com>
2025-07-02 23:52:33 +0300

    Cleanup

--- src/main/java/com/ktilelis/todo/TodoApplication.java
+++ src/main/java/com/ktilelis/todo/TodoApplication.java
@@ -2,15 +2,14 @@ package com.ktilelis.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 
 }
